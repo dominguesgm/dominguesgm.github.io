1214c91693a3c3a793a8e9ec16bf7b7df413055f
Gil Domingues <gil.domingues2@hotmail.com>
2019-11-02 00:36:38 +0000

    chore: include postcss-url to correct asset urls after postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	'plugins': [
 		require('postcss-import'),
 		require('postcss-mixins'),
+		require('postcss-url'),
 		require('postcss-preset-env')({
 			browsers: 'defaults',
 			stage: 3,
