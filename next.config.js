9a637fe9f07decb9b72c4cce55fb236266701bce
Gil Domingues <gil.domingues2@hotmail.com>
2019-11-02 00:33:18 +0000

    chore: add withFonts plugin to load local fonts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 
 
+const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
+const withFonts = require('next-fonts');
 
-module.exports = withCSS({
-	cssModules: true,
-	cssLoaderOptions: {
-		importLoaders: 1,
-		localIdentName: '[local]___[hash:base64:5]',
-	},
-});
+module.exports = withPlugins([
+	[
+		withCSS,
+		{
+			cssModules: true,
+			cssLoaderOptions: {
+				importLoaders: 1,
+				localIdentName: '[local]___[hash:base64:5]',
+			},
+		},
+	], [
+		withFonts,
+	],
+]);
