13c800de087393250c232d3e462b0298a15066a9
Gil Domingues <gil.domingues2@hotmail.com>
2020-01-27 13:48:02 +0000

    chore: added local cv file

--- src/shared/components/header/Header.js
+++ src/shared/components/header/Header.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import classNames from 'class-names';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import styles from './Header.css';
 
+import cv from '../../media/files/CV.pdf';
+
+console.log('cv', cv);
+
 const socialData = [
+	{
+		url: 'https://github.com/dominguesgm',
+		icon: faGithub,
+	},
 	{
 		url: 'https://www.linkedin.com/in/gildomingues',
 		icon: faLinkedin,
 	},
 	{
-		url: 'https://github.com/dominguesgm',
-		icon: faGithub,
+		url: cv,
+		icon: faFileAlt,
 	},
 ];
 
@@ -25,7 +34,11 @@ const Header = () => {
 
 	const renderedIcons = useMemo(() => (
 		socialData.map((element, index) => (
-			<a href={element.url} className={ classNames(styles.anchorIcon, entered && styles.entered) } style={{ 'transition-delay': `${index*0.2+2.5}s;` }}>
+			<a href={ element.url }
+				target="_blank"
+				key={ index }
+				className={ classNames(styles.anchorIcon, entered && styles.entered) }
+				style={{ 'transitionDelay': `${index*0.2+2.5}s` }}>
 				<FontAwesomeIcon icon={ element.icon } size="2x" className={ styles.icon }/>
 			</a>
 		))
