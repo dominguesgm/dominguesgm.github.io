a60e0533a8d0efd6e6ed35b66f6982807b0b07a4
Gil Domingues <gil.domingues2@hotmail.com>
2020-08-14 19:56:37 +0100

    chore: fix hero font size and add meta tags

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -37,6 +37,10 @@ class App extends BaseApp {
 					<title>
                         Gil Domingues
 					</title>
+					<meta name="description" content="Gil Domingues, a software engineer passionate technology, from great user experiences to interesting low level problems.">
+					<meta name=”robots” content="index, follow">
+					<meta charset="UTF-8">
+					<meta name="keywords" content="software, engineer, developer, Gil, Domingues, freelance" />
 				</Head>
 				<Component { ...pageProps } />
 			</>
